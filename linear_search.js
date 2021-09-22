91bde52f8c8bedde17b2b4e4ccd96d30a11386fd
BeWoler <bewoler.del@gmail.com>
2021-09-22 14:16:24 +0300

    linear search

---
+++ linear_search.js
@@ -0,0 +1,16 @@
+const arr = [1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 11];
+
+let count = 0;
+
+function linearSearch(arr, item) {
+  for (let i = 0; i < arr.length; i++) {
+    count++;
+    if (arr[i] === item) {
+      return i;
+    }
+  }
+  return null;
+}
+
+console.log(linearSearch(arr, 8));
+console.log(`count = ${count}`);
